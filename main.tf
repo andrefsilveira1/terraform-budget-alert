13b34062dbf3de53fe090e82a3fa33bfb09537fc
André Freitas <freitasandre38@gmail.com>
2024-01-11 16:40:19 -0300

    feat: create new resources to terraform infrastructure as code

--- main.tf
+++ main.tf
@@ -16,12 +16,55 @@ resource "azurerm_consumption_budget_subscription" "budget" {
     dynamic "notification" {
       for_each = each.value.notifications
       content {
-        enabled = notification.value.enabled
-        threshold = notification.value.threshold
-        operator = notification.value.operator
-        threshold_type = notification.value.threshold_type
-        contact_emails = notification.value.contact_emails
-        contact_roles = notification.value.contact_roles
+        enabled         = notification.value.enabled
+        threshold       = notification.value.threshold
+        operator        = notification.value.operator
+        threshold_type  = notification.value.threshold_type
+        contact_emails  = notification.value.contact_emails
+        contact_roles   = notification.value.contact_roles
       }
     }
+}
+
+# Novos recursos:
+
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name               = "example"
+  target_resource_id = azurerm_storage_account.example.id # substituir id
+
+  enabled_log {
+    category = "AuditEvent"
+
+    retention_policy { # Analisar depreciação de retention policy
+      enabled = false
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "metric_alert" {
+  name                = "metric_alert"
+  resource_group_name = azurerm_resource_group.metric_alert.name
+  scopes              = [azurerm_virtual_machine.metric_alert.id]
+  description         = "Metric alerts when got 80% of CPU usage"
+  enabled             = true
+
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name      = "Percentage CPU"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 80
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.metric_alert.id
+  }
 }
